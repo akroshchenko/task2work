e0adcfae9c469e2f54f7f4aec46680f2d9268bc3
Andrii Kroshchenko debian_test <akroshchenko@mirantis.com>
2016-03-17 20:40:27 +0200

    add print_body function

--- script2.sh
+++ script2.sh
@@ -2,8 +2,41 @@
 
 usage="script2.sh (sort|find) (version|date|vd|anom|error) name_input_file [name_output_file]"
 
+print_body(){
 
 
+
+local name=$(cut -d " " -f 1 <(echo $2))
+local version=$(cut -d " " -f 2 <(echo $2))
+
+
+
+flag=0
+while read -r line
+do
+if [ ${flag} ]
+then
+
+	if [[ $line =~ ^.*--  ]]
+	then 
+		echo "$line"
+		break
+	fi
+echo "$line"
+fi
+
+
+if [[ $line =~ .*${name}.*${version} ]]
+then
+echo "$line"
+flag=1 
+fi
+done<"$1"
+return 0
+}
+
+print_body $1 "mysql (3.20.32a-4)" 
+exit 0
 case $1 in
 	sort)
 		case $2 in
@@ -29,3 +62,5 @@ case $1 in
 	   *) echo "Error in the comand"
 	      echo "use next syntax $usage"
 esac
+
+
