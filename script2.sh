bdbb7415b1667a8c8eb663640280e328e89ccf11
Andrii Kroshchenko debian_test <akroshchenko@mirantis.com>
2016-03-21 21:15:54 +0200

    add sort_by_version (incomplete)

--- script2.sh
+++ script2.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-usage="script2.sh (sort|find) (version|date|vd|anom|error) name_input_file [name_output_file]"
+usage="script2.sh (sort|find) (version|date|vd|anom|error) name_input_file "
 
-print_body(){
+input_file=$3
+name_of_package=mysql
 
-
+print_body() {
 
 local name=$(cut -d " " -f 1 <(echo $2))
 local version=$(cut -d " " -f 2 <(echo $2))
-
-
-
+echo "start printbody"
 flag=0
 while read -r line
 do
 if [ ${flag} ]
 then
-
 	if [[ $line =~ ^.*--  ]]
 	then 
 		echo "$line"
@@ -25,22 +23,38 @@ then
 echo "$line"
 fi
 
-
 if [[ $line =~ .*${name}.*${version} ]]
 then
 echo "$line"
 flag=1 
 fi
-done<"$1"
+done<${input_file}
+echo "end of print body"
 return 0
 }
 
-print_body $1 "mysql (3.20.32a-4)" 
-exit 0
+sort_by_version(){
+old_IFS=${IFS}
+echo "start of sort and"
+
+
+
+#IFS=$'\n'
+for print_package in "$( grep "^${name_of_package}" ${input_file} | cut -d " " -f 2 |sort -r -k 2,2 | tr '\n' ' ')"
+do
+	IFS=$old_IFS
+	print_body input_file "${print_package}"
+
+done
+echo "end of sort"
+
+}
+
 case $1 in
-	sort)
+	sort)echo "sort option"
 		case $2 in
-			version) 
+			version)echo "sort by version" 
+				sort_by_version 
 			      ;;
 			   date)
 			      ;;
