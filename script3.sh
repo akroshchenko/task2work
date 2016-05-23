64e3c9f45cbb449516dd2691da4bc3bf2a71bcf6
Andrii Kroshchenko <akroshchenko@mirantis.com>
2016-05-23 19:24:13 +0300

    v3-rc1 (working example)

--- script3.sh
+++ script3.sh
@@ -5,7 +5,7 @@
 
 USAGE="script.sh CHANGELOG_FILE"
 
-INPUT_FILE="${1}"
+INPUT_FILE=$(find ./debian -name changelog)
 
 
 # This function takes a list of PACKAGEs which need to print. 
@@ -15,46 +15,31 @@ INPUT_FILE="${1}"
 
 
 look_for_format_errors(){
-
-FORMATING_ERROR=$(mktemp -t FORMATING_ERROR.XXXXX)
-
-if [ "$(head -n 1 ${INPUT_FILE})" == " " ];then
-	echo "File ${INPUT_FILE} contains empty first line"
-	exit 1
-fi
+while [ -z "$(head -n 1 ${INPUT_FILE})" ]; do
+	sed -i "1d" "${INPUT_FILE}"
+done
 GLOBAL_NAME_OF_PACKAGE="$(head -n 1 ${INPUT_FILE} |cut -d" " -f1|cut -d"-" -f1)"
-echo -E "look for the format errors =====>"
-grep -e "${GLOBAL_NAME_OF_PACKAGE}.*urgency.*" "${INPUT_FILE}" | grep -Ev "^${GLOBAL_NAME_OF_PACKAGE}.*[[:space:]]{1}\(([0-9]:)?[0-9\.[0-9]\..*" >>${FORMATING_ERROR} || true #debug
-grep -e "--.*>" "${INPUT_FILE}" |grep -vE "^[[:space:]]{1}--.*>[[:space:]]{2}(Mon, |Tue, |Wed, |Thu, |Fri, |Sat, |Sun)" >>${FORMATING_ERROR} ||true
- if [ -s ${FORMATING_ERROR} ]; then
- 	echo "Correct next formating errors:"
- 	cat ${FORMATING_ERROR}
- else
- 	echo "Nothing to fix"
- fi
- rm -rf ${FORMATING_ERROR}
+sed -i "/${GLOBAL_NAME_OF_PACKAGE}.*urgency.*/ s/^ *//" "${INPUT_FILE}"
+sed -i "/--.*>/ s/^ */ /" "${INPUT_FILE}"
 
 }
 
 check_epoch(){
-
 if grep "^${GLOBAL_NAME_OF_PACKAGE}" "${INPUT_FILE}" |grep -q ":" ; then
 	sed -i "${GLOBAL_NAME_OF_PACKAGE}.*([0-9][^:]/s/(/(0:/g" "${INPUT_FILE}"
 fi
-
 }
 
 print_body(){
-local LINE_1 LINE
+local LINE_1 LINE NAME VERSION FLAG
 local INPUT_ORDER=$1
-local NAME VERSION LINE_1 FLAG
-while IFS= read -r LINE_1; do
+while IFS= read LINE_1; do
 	if $(echo "${LINE_1}" |grep -q "^${GLOBAL_NAME_OF_PACKAGE}"); then
 		NAME="$( echo "${LINE_1}" |cut -d " " -f 1 )"
 		VERSION="$( echo "${LINE_1}" |cut -d " " -f 2 | sed -e "s/\((\|)\)//g")"
 		FLAG=0
 	fi
-	while IFS= read -r LINE; do
+	while IFS= read LINE; do
 		if [ "${FLAG}" -eq 1 ]; then
 			if echo "${LINE}" | grep "<.*@.*>" | grep -qe "^ -- "; then
 				echo "${LINE}"
@@ -69,7 +54,6 @@ while IFS= read -r LINE_1; do
 		fi
 	done<${INPUT_FILE}
 done<${INPUT_ORDER}
-
 }
 
 
@@ -79,9 +63,9 @@ TEMP_BUFFER=$(mktemp -t TEMP_BUFFER.XXXXX)
 MY_VER_SORT_TEMP=$(mktemp -t MY_VER_SORT_TEMP.XXXXX)
 SORTING_ORDER_INPUT_FILE=$(mktemp -t SORTING_ORDER_INPUT_FILE.XXXXX)
 DIFF_FILE=$(mktemp -t DIFF_FILE.XXXXX)
-echo "$(grep "^${GLOBAL_NAME_OF_PACKAGE}" "${INPUT_FILE}" | cut -d " " -f 1,2 | sort -r | sed -e "s/\((\|)\)//g")" > "${MY_VER_SORT_TEMP}"
-dpkg-parsechangelog -l changelog --show-field version --all --format rfc822| sed '/^$/d' > "${SORTING_ORDER_INPUT_FILE}"
-diff "${SORTING_ORDER_INPUT_FILE}" <(cut -d" " -f2 "${MY_VER_SORT_TEMP}") > "${DIFF_FILE}"
+echo "$(grep "^${GLOBAL_NAME_OF_PACKAGE}" "${INPUT_FILE}" | cut -d " " -f 1,2 | sort -ru| sed -e "s/\((\|)\)//g")" > "${MY_VER_SORT_TEMP}"
+dpkg-parsechangelog --show-field version --all --format rfc822| sed '/^$/d' > "${SORTING_ORDER_INPUT_FILE}"
+diff -u "${SORTING_ORDER_INPUT_FILE}" <(cut -d" " -f2 "${MY_VER_SORT_TEMP}") > "${DIFF_FILE}"
 if [ -s ${DIFF_FILE} ]; then
 	echo "${INPUT_FILE} has problem whith sorting package version"
 	cat ${DIFF_FILE}
@@ -90,6 +74,7 @@ print_body "${MY_VER_SORT_TEMP}" > "${TEMP_BUFFER}"
 cp "${TEMP_BUFFER}" "${INPUT_FILE}"
 # rm -rf "${MY_VER_SORT_TEMP}" "${SORTING_ORDER_INPUT_FILE}" "${TEMP_BUFFER}" "${DIFF_FILE}"
 echo "${MY_VER_SORT_TEMP}" "${SORTING_ORDER_INPUT_FILE}" "${TEMP_BUFFER}" "${DIFF_FILE}"
+
 }
 
 
@@ -101,20 +86,20 @@ i=0
 j=0
 while read true ; do
 	i=$((i + 1))
-	if dpkg-parsechangelog -l changelog -o $i -c 1 --show-field source --format rfc822 &>/dev/null ; then
+	if dpkg-parsechangelog -o $i -c 1 --show-field source --format rfc822 &>/dev/null ; then
 		break
 	fi
-	VERSION_1="$(dpkg-parsechangelog -l changelog -n $i --show-field version --format rfc822)"
-	DATE_1="$(dpkg-parsechangelog -l changelog -n $i --show-field date --format rfc822)"
+	VERSION_1="$(dpkg-parsechangelog -n $i --show-field version --format rfc822)"
+	DATE_1="$(dpkg-parsechangelog -n $i --show-field date --format rfc822)"
 	while read true; do
 		j=$((i + 1))
-		if dpkg-parsechangelog -l changelog -o $j -c 1 --show-field source --format rfc822 &>/dev/null ; then
+		if dpkg-parsechangelog -o $j -c 1 --show-field source --format rfc822 &>/dev/null ; then
 			break
 		fi
-		VERSION_2="$(dpkg-parsechangelog -l changelog -n $j --show-field version --format rfc822)"
-		DATE_2="$(dpkg-parsechangelog -l changelog -n $j --show-field date --format rfc822)"
+		VERSION_2="$(dpkg-parsechangelog -n $j --show-field version --format rfc822)"
+		DATE_2="$(dpkg-parsechangelog -n $j --show-field date --format rfc822)"
 		if dpkg --compare-versions "${VERSION_1}" gt "${VERSION_2}" ; then
-			if [ date -d ${DATE_1} +%s -gt date -d ${DATE_2} +%s ]; then
+			if [ `date -d ${DATE_1} +%s` -gt `date -d ${DATE_2} +%s` ]; then
 				continue
 			else
 				echo "anomaly is in a date between :"
@@ -122,7 +107,7 @@ while read true ; do
 				exit 1
 			fi
 		elif dpkg --compare-versions "${VERSION_1%~*}" ge "${VERSION_2}" ; then
-			if [ date -d ${DATE_1} +%s -gt date -d ${DATE_2} +%s ]; then
+			if [ `date -d ${DATE_1} +%s` -gt `date -d ${DATE_2} +%s` ]; then
 				continue
 			else
 				echo "anomaly is in a date between :"
@@ -143,7 +128,6 @@ sed -i "/^${GLOBAL_NAME_OF_PACKAGE}.*(0:/s/(0:/(/g" "${INPUT_FILE}"
 }
 
 # the logic of script
-
 
 look_for_format_errors
 check_epoch
